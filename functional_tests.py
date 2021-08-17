f4945041758717ee361ff4671d80ccfc0a7cde1c
Vlad <Promotion-kmv@yandex.ru>
2021-08-17 08:58:34 +0300

    refactoring functional test

--- functional_tests.py
+++ functional_tests.py
@@ -16,25 +16,30 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
-        time.sleep(2)
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('Hello', header_text)
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        input_box = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            inputbox.get_attribute('placeholder'),
+            input_box.get_attribute('placeholder'),
             'Enter a to-do item'
         )
-        inputbox.send_keys('Купить павлиньи перья')
-        inputbox.send_keys(Keys.ENTER)
+        input_box.send_keys('Купить павлиньи перья')
+        input_box.send_keys(Keys.ENTER)
         time.sleep(2)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Купить павлиньи перья' for row in rows),
-            "Новый элемент списка не появился в таблице"
+        self.assertIn('1: Купить павлиньи перья', [row.text for row in rows])
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Сделать мушку из павлиньих перьев')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(2)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(
+            '2 : Сделать мушку из павлиних перьев',
+            [row.text for row in rows]
         )
-
 
 
 if __name__ == '__main__':
--- study_tdd/views.py
+++ study_tdd/views.py
@@ -1,7 +1,9 @@
-#page 48 TDD
-
+#page 98 TDD
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
