924a0b1eabe953a4cc35f9536aa30912398b4dd1
Vlad <Promotion-kmv@yandex.ru>
2021-08-28 21:13:01 +0300

    moving on

--- study_tdd/urls.py
+++ study_tdd/urls.py
@@ -2,8 +2,10 @@ from django.urls import path, include
 from .views import *
 
 app_name = 'english_teacher'
-
 urlpatterns = [
     path('', index, name='index'),
-    path('lists/', view_list, name='view_list'),
+    path('lists/new', new_list, name='new_list'),
+    path('lists/one/', view_list, name='view_list'),
+
 ]
+
--- study_tdd/views.py
+++ study_tdd/views.py
@@ -1,17 +1,21 @@
-#page 146 TDD
+#page 151 TDD
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import *
 
 
 def index(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/')
-    items = Item.objects.all()
-    return render(request, 'index.html', {'items': items})
+    # if request.method == 'POST':
+    #     Item.objects.create(text=request.POST['item_text'])
+    #     return redirect('/lists/one/')
+    # items = Item.objects.all()
+    return render(request, 'index.html')
 
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/one/')
